f3ca8208d018fd0cc75a661139295f2b40bea74f
thepown <felipe.medina.o@gmail.com>
2014-12-15 17:49:40 -0500

    change password

--- authemail/views.py
+++ authemail/views.py
@@ -217,11 +217,11 @@ class PasswordChange(APIView):
             user.set_password(password)
             user.save()
 
-            content = {'success': 'Password changed.'}
+            content = {'detail': 'success'}
             return Response(content, status=status.HTTP_200_OK)
 
         else:
-            return Response(serializer.errors, 
+            return Response({'detail':'data_error','errors':serializer.errors}, 
                 status=status.HTTP_400_BAD_REQUEST)
 
 
